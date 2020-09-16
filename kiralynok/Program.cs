e4d05fd5a9a3693ce9f399c0b9e5427094a7c468
FabianTamas <fabiant0130@gmail.com>
2020-09-16 08:34:56 +0200

    Add project files.

---
+++ kiralynok/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiralynok
+{
+    class Tabla
+    {
+        private char[,] T = new char[8,8];
+        private char Urescella;
+        private int UresOszlopokSzama;
+        private int UresSorokSzama;
+        
+        public Tabla(char ch)
+        {
+
+        }
+
+        public void Elhelyez()
+        {
+
+        }
+
+        public void FajlbaIr()
+        {
+
+        }
+
+        public void Megjelenit()
+        {
+
+        }
+
+        public int UresOszlop()
+        {
+
+        }
+
+        public int UresSor()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elhelyez();
+            FajlbaIr();
+            Megjelenit();
+            Tabla();
+            UresOszlop();
+            UresSor();
+
+
+            Console.ReadKey();
+        }
+    }
+}
